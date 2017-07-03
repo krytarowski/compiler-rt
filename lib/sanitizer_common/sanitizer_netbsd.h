4015724c9dcff7bf722514fc184788ee4e0544be
Kamil Rytarowski <n54@gmx.com>
2017-07-03 20:00:33 +0200

    2

---
+++ lib/sanitizer_common/sanitizer_netbsd.h
@@ -0,0 +1,20 @@
+//===-- sanitizer_freebsd.h -------------------------------------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file is a part of Sanitizer runtime. It contains NetBSD-specific
+// definitions.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef SANITIZER_NETBSD_H
+#define SANITIZER_NETBSD_H
+
+#include "sanitizer_internal_defs.h"
+
+#endif  // SANITIZER_NETBSD_H
