74f15737a67d2152d21879203f4a88691290d6eb
Kamil Rytarowski <n54@gmx.com>
2019-09-11 02:58:09 +0200

    Split many_tls_keys.cpp into two tests
    many_tls_keys_pthread.cpp for TSD
    many_tls_keys_thread.cpp for TLS
    
    The TSD test is unsupported on NetBSD as it assumes TLS used internally.

--- test/lsan/TestCases/many_tls_keys_thread.cpp
+++ test/lsan/TestCases/many_tls_keys_thread.cpp
@@ -1,8 +1,9 @@
 // Test that lsan handles tls correctly for many threads
 // RUN: LSAN_BASE="report_objects=1:use_stacks=0:use_registers=0"
-// RUN: %clangxx_lsan %s -DUSE_PTHREAD -o %t-pthread
-// RUN: %env_lsan_opts=$LSAN_BASE:"use_tls=0" not %run %t-pthread 2>&1 | FileCheck %s
-
+// RUN: %clangxx_lsan %s -DUSE_THREAD -o %t
+// RUN: %env_lsan_opts=$LSAN_BASE:"use_tls=0" not %run %t 2>&1 | FileCheck %s
+// RUN: %env_lsan_opts=$LSAN_BASE:"use_tls=1" %run %t 2>&1
+// RUN: %env_lsan_opts="" %run %t 2>&1
 
 // Patch r303906 did not fix all the problems.
 // UNSUPPORTED: arm-linux,armhf-linux
@@ -19,24 +20,18 @@ pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 int finished = 0;
 
-// We won't be able to create the maximum number of keys, due to other users
-// of the tls, but we'll use as many keys as we can before failing to create
-// a new key.
-pthread_key_t keys[PTHREAD_KEYS_MAX];
-static const int PTHREAD_KEY_INVALID = 0xffffffff;
+__thread void *ptr1;
+__thread void *ptr2;
+__thread void *ptr3;
+__thread void *ptr4;
+__thread void *ptr5;
 
 void alloc() {
-  for (int i = 0; i < PTHREAD_KEYS_MAX; ++i) {
-    void *ptr = malloc(123);
-    if ((keys[i] == PTHREAD_KEY_INVALID) || pthread_setspecific(keys[i], ptr)) {
-      free(ptr);
-      break;
-    }
-  }
-}
-
-void pthread_destructor(void *arg) {
-  assert(0 && "pthread destructors shouldn't be called");
+  ptr1 = malloc(1111);
+  ptr2 = malloc(2222);
+  ptr3 = malloc(3333);
+  ptr4 = malloc(4444);
+  ptr5 = malloc(5555);
 }
 
 void *thread_start(void *arg) {
@@ -52,13 +47,6 @@ void *thread_start(void *arg) {
 }
 
 int main() {
-  for (int i = 0; i < PTHREAD_KEYS_MAX; ++i) {
-    if (pthread_key_create(&keys[i], pthread_destructor)) {
-      keys[i] = PTHREAD_KEY_INVALID;
-      break;
-    }
-  }
-
   pthread_t thread[NUM_THREADS];
   for (int i = 0; i < NUM_THREADS; ++i) {
     assert(0 == pthread_create(&thread[i], 0, thread_start, 0));
